aca8e214333e9120ba36586509fa91f5b97e3dc1
goodyngoddy <goodnewsngodspower@gmail.com>
2025-06-11 18:02:29 +0100

    update

--- toggle.js
+++ toggle.js
@@ -9,7 +9,11 @@ const navLandBtns = document.querySelectorAll('.landing .nav-item')
 const landForm = document.querySelector('.landing .footer form')
 const xLandPop = document.querySelector('.landing .pop-up .btn')
 
+//signer
 
+
+
+//landing
 function moveToSigner(param, i) {
 	param.addEventListener('click', () => {
 		app.classList.toggle('user')
@@ -18,16 +22,18 @@ function moveToSigner(param, i) {
 }
 entryBtn.forEach((entryBtn, i) => moveToSigner(entryBtn, i))
 navLandBtns.forEach((navLandBtns, i) => moveToSigner(navLandBtns, i))
-
 signerSwitch.addEventListener('click', () => signer.classList.toggle('log'))
-
 signerBack.addEventListener('click', () => {
 	app.classList.toggle('user')
 	if (signer.classList.contains('log')) signer.classList.toggle('log')
 })
-
 landForm.addEventListener('submit', (e) => {
 	e.preventDefault()
 	app.classList.toggle('pop')
 })
-xLandPop.addEventListener('click', () => app.classList.toggle('pop'))
+xLandPop.addEventListener('click', () => {
+	app.classList.toggle('pop')
+	window.open('/')
+})
+
+
