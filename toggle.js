579cb9cadc341f9750812f98377d08a7e0f1e5fa
goodyngoddy <goodnewsngodspower@gmail.com>
2025-06-11 15:06:22 +0100

    update

--- toggle.js
+++ toggle.js
@@ -1,13 +1,13 @@
+//landing
 const app = document.querySelector('.app')
 const signer = document.querySelector('.signer')
-
 
 const signerSwitch = document.querySelector('.signer .switch')
 const signerBack = document.querySelector('.signer .back')
 const entryBtn = document.querySelectorAll('.sign-btns .btn')
 const navLandBtns = document.querySelectorAll('.landing .nav-item')
-
-
+const landForm = document.querySelector('.landing .footer form')
+const xLandPop = document.querySelector('.landing .pop-up .btn')
 
 
 function moveToSigner(param, i) {
@@ -16,9 +16,7 @@ function moveToSigner(param, i) {
 		if (i) signer.classList.toggle('log')
 	})
 }
-
 entryBtn.forEach((entryBtn, i) => moveToSigner(entryBtn, i))
-
 navLandBtns.forEach((navLandBtns, i) => moveToSigner(navLandBtns, i))
 
 signerSwitch.addEventListener('click', () => signer.classList.toggle('log'))
@@ -27,3 +25,9 @@ signerBack.addEventListener('click', () => {
 	app.classList.toggle('user')
 	if (signer.classList.contains('log')) signer.classList.toggle('log')
 })
+
+landForm.addEventListener('submit', (e) => {
+	e.preventDefault()
+	app.classList.toggle('pop')
+})
+xLandPop.addEventListener('click', () => app.classList.toggle('pop'))
