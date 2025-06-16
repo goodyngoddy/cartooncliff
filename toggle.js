78b3ac8becac17df9f207973196415ecc2b59149
goodyngoddy <goodnewsngodspower@gmail.com>
2025-06-16 19:25:11 +0100

    update

--- toggle.js
+++ toggle.js
@@ -11,7 +11,26 @@ const xLandPop = document.querySelector('.landing .pop-up .btn')
 
 //signer
 
+const loginData = {
+	username: 'username',
+	password: 'password'
+}
 
+const username = document.querySelector('.signer .log-in-el #username')
+const password = document.querySelector('.signer .log-in-el #password')
+const logInForm = document.querySelector('.signer .log-in-el form')
+
+
+logInForm.addEventListener('submit', (e) => {
+	e.preventDefault()
+	
+	if (username.value === loginData.username && password.value === loginData.password) {
+		app.classList.toggle('lin')
+		app.classList.toggle('user')
+	} else {
+		alert('Invalid credentials')
+	}
+})
 
 //landing
 function moveToSigner(param, i) {
@@ -21,7 +40,7 @@ function moveToSigner(param, i) {
 	})
 }
 entryBtn.forEach((entryBtn, i) => moveToSigner(entryBtn, i))
-navLandBtns.forEach((navLandBtns, i) => moveToSigner(navLandBtns, i))
+navLandBtns.forEach((navLandBtn, i) => moveToSigner(navLandBtn, i))
 signerSwitch.addEventListener('click', () => signer.classList.toggle('log'))
 signerBack.addEventListener('click', () => {
 	app.classList.toggle('user')
@@ -36,4 +55,27 @@ xLandPop.addEventListener('click', () => {
 	window.open('/')
 })
 
+//app main
+const appBody = document.querySelector('.app-body')
+const myPics = document.querySelector('.wrk-btn')
+const appNavBtns = document.querySelectorAll('.foot-bar .ft-item')
+const meNavBtns = document.querySelectorAll('.cont-me ul li')
+
+
+myPics.addEventListener('click', () => appBody.classList.toggle('me'))
+
+appNavBtns.forEach((appNavBtn, i) => {
+	appNavBtn.addEventListener('click', () => {
+		if (i) {
+			if (1 == 1) appBody.classList.remove('me')
+			if (i == 2 && !appBody.classList.contains('me')) appBody.classList.add('me')
+		}
+	})
+})
+
+meNavBtns.forEach((meNavBtn, i) => {
+	meNavBtn.addEventListener('click', () => {
+		console.log(meNavBtn.textContent)
+	})
+})
 
